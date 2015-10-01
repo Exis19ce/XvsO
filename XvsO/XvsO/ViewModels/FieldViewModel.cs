e2f3293b8a26d8bcbc2b3989dafe7906af4e6892
exis19ce <exis19ce@bk.ru>
2015-10-01 18:05:21 +0300

    Create project Create View and first step in viewModel

---
+++ XvsO/XvsO/ViewModels/FieldViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace XvsO
+{
+	public class FieldViewModel
+	{
+
+		private List<string> ListX = new List<string> ();
+		private List<string> ListO = new List<string> ();
+		private bool BoolForStep = true;
+
+
+		public FieldViewModel ()
+		{
+			CommandButtonClicked = new Command<string> (ButtonClicked);
+		}
+
+		public ICommand CommandButtonClicked { get; private set; }
+
+		private void ButtonClicked (string value)
+		{
+			if (BoolForStep) {
+				BoolForStep = false;
+				ListX.Add (value);
+				CheckWinner (ListX);
+			} else {
+				BoolForStep = true;
+				ListO.Add (value);
+				CheckWinner (ListO);
+			}
+		}
+
+		private void CheckWinner (List<String> list)
+		{
+			if (list.Contains ("1") || list.Contains ("2") || list.Contains ("3")) {
+			} else if (list.Contains ("4") || list.Contains ("5") || list.Contains ("6")) {
+				
+			} else if (list.Contains ("7") || list.Contains ("8") || list.Contains ("9")) {
+				
+			} else if (list.Contains ("1") || list.Contains ("4") || list.Contains ("7")) {
+				
+			} else if (list.Contains ("2") || list.Contains ("5") || list.Contains ("8")) {
+								
+			} else if (list.Contains ("3") || list.Contains ("6") || list.Contains ("9")) {
+				
+			} else if (list.Contains ("1") || list.Contains ("5") || list.Contains ("9")) {
+					
+			} else if (list.Contains ("3") || list.Contains ("5") || list.Contains ("7")) {
+						
+			} else {
+				var asdas = 33;
+			}
+		}
+
+	}
+}
+
+
---
+++ XvsO/XvsO/Views/FieldPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XvsO
+{
+	public partial class FieldPage : ContentPage
+	{
+		public FieldPage ()
+		{
+			InitializeComponent ();
+			BindingContext = new FieldViewModel ();
+			Padding = new Thickness (0, Device.OnPlatform (20, 0, 0), 0, 0);
+		}
+	}
+}
+
